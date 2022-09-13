6e3d592ba5c9072c71a8d5df479e4dea30a199a0
Julia Semenyuk <julia_semenyuk@internet.ru>
2022-09-14 01:20:38 +0900

    Initial Commit

---
+++ 2.30 HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.30 HW1
+//
+//  Created by Юлия on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
